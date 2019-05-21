3523e46f05865052950bb75a0f6b062dc8aff543
tenythomas01 <49268929+tenythomas01@users.noreply.github.com>
2019-05-21 16:02:53 +0530

    assests added

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject loadMenu;
+
+
+    public void HideMainMenu() {
+        mainMenu.SetActive(false);
+    }
+
+    public void ShowMainMenu() {
+        mainMenu.SetActive(true);
+    }
+
+    public void ContinueGame() {
+        Debug.Log("Continue");
+    }
+
+    public void ShowOptionsMenu() {
+        HideMainMenu();
+        optionsMenu.SetActive(true);
+    }
+
+    public void HideOptionsMenu() {
+        optionsMenu.SetActive(false);
+        ShowMainMenu();
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
